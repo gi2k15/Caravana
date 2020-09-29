99ff844dcfbe668bc14e39f275aa16a58566ad24
gi2k15 <gendouikari2015@gmail.com>
2020-09-29 15:08:29 -0300

    Some changes thinking about Shadowlands
    * Better event handler (thinking about some libraries later)
    * Better addon description
    * Changed global variable to avoid possible conflicts

--- caravana.lua
+++ caravana.lua
@@ -2,22 +2,31 @@
 -- Return to Camp = 312372
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-f:SetScript("OnEvent", function(self, event, ...)
+
+local events = {}
+
+function events:UNIT_SPELLCAST_SUCCEEDED(...)
 	local _,_,spellID = ...
 	if spellID == 312370 then
 		local subzone, zone = GetSubZoneText(), GetZoneText()
 		if subzone == "" then
-			CaravanDB = zone
+			CaravanaDB = zone
 		else
-			CaravanDB = string.format("%s, %s", subzone, zone)
+			CaravanaDB = string.format("%s, %s", subzone, zone)
 		end
 	end
+end
+
+f:SetScript("OnEvent", function(self, event, ...)
+	events[event](self, ...)
 end)
+for k, v in pairs(events) do
+	f:RegisterEvent(k)
+end
 
 GameTooltip:HookScript("OnTooltipSetSpell", function(self)
 	local _,SpellID = self:GetSpell()
-	if SpellID == 312372 and CaravanDB then
-		self:AddLine("@ " .. CaravanDB,0.94,0.9,0.55,true)
+	if SpellID == 312372 and CaravanaDB then
+		self:AddLine("@ " .. CaravanaDB,0.94,0.9,0.55,true)
 	end
 end)
